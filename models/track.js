d82ad1d9157e141706eda09a11dc3b5cf2315e4a
Francis Rodson Clavel <frdclav@gmail.com>
2020-01-28 15:36:15 -0800

    creates track.js model

---
+++ models/track.js
@@ -0,0 +1,15 @@
+module.exports = function (sequelize, DataTypes) {
+  var Track = sequelize.define("Track", {
+    title: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    instrument: DataTypes.STRING,
+    length: DataTypes.INTEGER,
+    genre: DataTypes.TEXT,
+    bpm: DataTypes.INTEGER,
+    key_signature: DataTypes.TEXT,
+    time_signature: DataTypes.TEXT,
+    sound_file: DataTypes.TEXT,
+
+  });
+  return Example;
+};
