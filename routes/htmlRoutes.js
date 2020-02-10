0cf83a017f56722f757b23d99caba2d46a45027f
Francis Rodson Clavel <frdclav@gmail.com>
2020-02-10 13:38:19 -0800

    home points to user specific tracks, explore points to all tracks uploaded

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -16,7 +16,7 @@ module.exports = function (app) {
       ]
     }).then(function (data) {
 
-      res.render("index", {
+      res.render("index_with_id", {
         tracks: data
       });
     });
@@ -29,7 +29,7 @@ module.exports = function (app) {
       ]
     }).then(function (data) {
 
-      res.render("index", {
+      res.render("explore", {
         tracks: data
       });
     });
